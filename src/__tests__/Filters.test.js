556303ce86dbf8b17cd50e09c9f1496573decb32
margaret wambui ndung'u <wambuindungu4967@gmail.com>
2024-08-26 12:27:14 +0400

    staged

--- src/__tests__/Filters.test.js
+++ src/__tests__/Filters.test.js
@@ -1,24 +1,29 @@
 import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
-import Filters from "../components/Filters";
 
-const FILTERS_STATE = {
-  type: "all",
-};
+function Filters({ onChangeType, onFindPetsClick }) {
+  function handleChange(event) {
+    onChangeType(event.target.value);
+  }
 
-test("calls the `onChangeType` prop callback when the animal type select changes", () => {
-  const onChangeType = jest.fn();
-  render(<Filters onChangeType={onChangeType} filters={FILTERS_STATE} />);
-  const select = screen.queryByLabelText("type");
-  fireEvent.change(select, { target: { value: "dog" } });
-  expect(onChangeType).toHaveBeenCalled();
-});
+  return (
+    <div className="ui form">
+      <h3>Animal type</h3>
+      <div className="field">
+        <select name="type" id="type" aria-label="type" onChange={handleChange}>
+          <option value="all">All</option>
+          <option value="cat">Cats</option>
+          <option value="dog">Dogs</option>
+          <option value="micropig">Micropigs</option>
+        </select>
+      </div>
 
-test('calls the `onFindPetsClick` callback prop when the "Find pets" button is clicked', () => {
-  const onFindPetsClick = jest.fn();
-  render(<Filters onFindPetsClick={onFindPetsClick} filters={FILTERS_STATE} />);
-  const button = screen.queryByText(/Find pets/g);
-  fireEvent.click(button);
-  expect(onFindPetsClick).toHaveBeenCalled();
-});
+      <div className="field">
+        <button onClick={onFindPetsClick} className="ui secondary button">
+          Find pets
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Filters;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
+import ReactDOM from "react-dom"
 import App from "./components/App";
 import "semantic-ui-css/semantic.min.css";
 
-import { createRoot } from 'react-dom/client';
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App tab="home" />);
+ReactDOM.render(<App/>, document.getElementById("root"));
